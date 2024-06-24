22e10dafcca596238d63d62884ad8d18837118c2
Huanxin Sheng <111191410+BruceSheng1202@users.noreply.github.com>
2024-06-24 14:09:39 +0800

    Update GetWeights_TE.m

--- constraints solution/GetWeights_TE.m
+++ constraints solution/GetWeights_TE.m
@@ -1,4 +1,5 @@
 function w = GetWeights_TE(inv_Sigma, mu, Xi)
+    %It's worth mentioning that the weights we get here is an adjusted version, which means it should be added by tracking index b then becomes the real one.
     %inv_Sigma is the estimated precision matrix
     %mu is the mean of return
     %Xi is risk aversion factor
