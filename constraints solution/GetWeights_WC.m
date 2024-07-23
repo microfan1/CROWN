71c7c039843e816eebc3776b6cda04046554eaf0
Huanxin Sheng <111191410+BruceSheng1202@users.noreply.github.com>
2024-07-23 16:27:57 +0800

    Update GetWeights_WC.m

--- constraints solution/GetWeights_WC.m
+++ constraints solution/GetWeights_WC.m
@@ -1,10 +1,9 @@
-function w = GetWeights_WC(inv_Sigma, mu, wx, delta, oneR)
-    %It's worth mentioning that the weights we get here is an adjusted version, which means it should be added by tracking index b then becomes the real one.
+function w = GetWeights_WC(inv_Sigma, mu, omega, delta, oneR, b)
     %inv_Sigma is the estimated precision matrix
     %mu is the mean of return
-    %delta is the risk aversion parameter
-    %oneR is the selected constrained stocks list
-    %wx(omega) is the weights sum of selected stocks;
+    %delta is the aversion parameter of risk
+    %oneR is the selected stocks list
+    %omega is the weights sum of selected stocksOne = ones(size(mu));
     One = ones(size(mu));
     a=inv_Sigma*One/(One'*inv_Sigma*One);
     B1 = One'*inv_Sigma*mu;
@@ -14,5 +13,6 @@ function w = GetWeights_WC(inv_Sigma, mu, wx, delta, oneR)
     wa = oneR'*inv_Sigma*One/(One'*inv_Sigma*One);
     L = (k-a)/(wk - wa);
     wu = oneR'*inv_Sigma*mu/(One'*inv_Sigma*mu) - wa;
-    w = (kw*(inv_Sigma*mu/B1-a)+(wx-kw*wu)*L) + (a-L*wa);
+    wx = omega - oneR'*b;
+    w = (kw*(inv_Sigma*mu/B1-a)+(wx-kw*wu)*L) + (a-L*wa) + b;
 end
