1f9c4ed694d52e64fa3319b0bfbd43d3957829ce
Huanxin Sheng <111191410+BruceSheng1202@users.noreply.github.com>
2024-07-23 18:03:47 +0800

    Update GetWeights_NC.m

--- constraints solution/GetWeights_NC.m
+++ constraints solution/GetWeights_NC.m
@@ -1,4 +1,4 @@
-function w = GetWeights_NC(inv_Sigma, mu, delta)
+function w = GetWeights_NC(inv_Sigma, mu, delta, m)
     %It's worth mentioning that the weights we get here is an adjusted version, which means it should be added by tracking index b then becomes the real one.
     %This function is used to get weights under no constraints 
     %inv_Sigma is the estimated precision matrix
@@ -8,5 +8,5 @@ function w = GetWeights_NC(inv_Sigma, mu, delta)
     a=inv_Sigma*One/(One'*inv_Sigma*One);
     B1 = One'*inv_Sigma*mu;
     kw = One'*inv_Sigma*mu/delta;
-    w = kw*(inv_Sigma*mu/B1-a) + a;
+    w = kw*(inv_Sigma*mu/B1-a) + a + m;
 end
