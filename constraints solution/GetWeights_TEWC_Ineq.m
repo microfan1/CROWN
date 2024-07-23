a77a8ba4d528c21ae6fc72ee62c6918ef708a41d
Huanxin Sheng <111191410+BruceSheng1202@users.noreply.github.com>
2024-07-23 20:42:03 +0800

    Update GetWeights_TEWC_Ineq.m

--- constraints solution/GetWeights_TEWC_Ineq.m
+++ constraints solution/GetWeights_TEWC_Ineq.m
@@ -1,4 +1,4 @@
-function w = GetWeights_TEWC_Ineq(inv_Sigma, mu, Xi, oneR, omega, b)
+function w = GetWeights_TEWC_Ineq(inv_Sigma, mu, Xi, oneR, wx, m)
     %inv_Sigma is the estimated precision matrix
     %mu is the mean of return
     %Xi is risk aversion factor
@@ -8,16 +8,16 @@ function w = GetWeights_TEWC_Ineq(inv_Sigma, mu, Xi, oneR, omega, b)
     a=inv_Sigma*One/(One'*inv_Sigma*One);
     t=inv_Sigma*mu/(One'*inv_Sigma*mu);
     u=t-a;
-    theta=One'*inv_Sigma*mu/Xi;
-    w0=theta*u;
+    kappa=One'*inv_Sigma*mu/Xi;
+    w0=kappa*u;
     k = inv_Sigma*oneR/(One'*inv_Sigma*oneR);
     wk = oneR'*inv_Sigma*oneR/(oneR'*inv_Sigma*One);
     wa = oneR'*inv_Sigma*One/(One'*inv_Sigma*One);
     L = (k-a)/(wk - wa);
     wu = oneR'*inv_Sigma*mu/(One'*inv_Sigma*mu) - wa;
-    if theta*wu >= omega
-        w = (omega - theta*wu)*L + w0 + b;
+    if kappa*wu >= wx
+        w = (wx - kappa*wu)*L + w0 + m;
     else
-        w = w0 + b;
+        w = w0 + m;
     end
 end
