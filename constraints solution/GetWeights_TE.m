49ff45a48b65756900895fdfaaef2a2e6c62aa82
Huanxin Sheng <111191410+BruceSheng1202@users.noreply.github.com>
2024-07-23 18:21:43 +0800

    Update GetWeights_TE.m

--- constraints solution/GetWeights_TE.m
+++ constraints solution/GetWeights_TE.m
@@ -1,4 +1,4 @@
-function w = GetWeights_TE(inv_Sigma, mu, Xi, b)
+function w = GetWeights_TE(inv_Sigma, mu, Xi, m)
     %inv_Sigma is the estimated precision matrix
     %mu is the mean of return
     %Xi is risk aversion factor
@@ -7,5 +7,5 @@ function w = GetWeights_TE(inv_Sigma, mu, Xi, b)
     t=inv_Sigma*mu/(One'*inv_Sigma*mu);
     u=t-a;
     kappa=One'*inv_Sigma*mu/Xi;
-    w=kappa*u + b;
+    w=kappa*u + m;
 end
