50d082bbea51aacaefd6bceeed1b131d867ff14a
Michael Fan <michaelqfan@gmail.com>
2024-07-24 08:09:54 +0800

    Update GetWeights_TE.m

--- constraints solution/GetWeights_TE.m
+++ constraints solution/GetWeights_TE.m
@@ -2,6 +2,7 @@ function w = GetWeights_TE(inv_Sigma, mu, Xi, m)
     %inv_Sigma is the estimated precision matrix
     %mu is the mean of return
     %Xi is risk aversion factor
+    % m is the targeted index, e.g., equal/mv weight, known indices s&p 500, Russell 2000, etc.
     One = ones(size(mu));
     a=inv_Sigma*One/(One'*inv_Sigma*One);
     t=inv_Sigma*mu/(One'*inv_Sigma*mu);
